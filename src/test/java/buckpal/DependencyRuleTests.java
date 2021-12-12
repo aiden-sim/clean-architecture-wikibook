bd26496e3c8e3684dce78dbebd2a088fd3729578
aiden.sim <aiden.sim@kakaocorp.com>
2021-12-12 21:34:57 +0900

    archunit

---
+++ src/test/java/buckpal/DependencyRuleTests.java
@@ -0,0 +1,45 @@
+package buckpal;
+
+import buckpal.archunit.HexagonalArchitecture;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+class DependencyRuleTests {
+
+    @Test
+    void validateRegistrationContextArchitecture() {
+        HexagonalArchitecture.boundedContext("buckpal")
+
+                .withDomainLayer("domain")
+
+                .withAdaptersLayer("adapter")
+                .incoming("in.web")
+                .outgoing("out.persistence")
+                .and()
+
+                .withApplicationLayer("application")
+                .services("service")
+                .incomingPorts("port.in")
+                .outgoingPorts("port.out")
+                .and()
+
+                .withConfiguration("configuration")
+                .check(new ClassFileImporter()
+                               .importPackages("buckpal.."));
+    }
+
+    @Test
+    void testPackageDependencies() {
+        noClasses()
+                .that()
+                .resideInAPackage("io.reflectoring.reviewapp.domain..")
+                .should()
+                .dependOnClassesThat()
+                .resideInAnyPackage("io.reflectoring.reviewapp.application..")
+                .check(new ClassFileImporter()
+                               .importPackages("io.reflectoring.reviewapp.."));
+    }
+
+}
